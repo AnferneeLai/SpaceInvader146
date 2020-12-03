b4d904161c08cbe520da4b6a485ac3331e1a3608
Anfernee Lai <anferneelai1129@gmail.com>
2020-12-02 19:14:38 -0800

    movement and shooting

--- Space Invader 146/Assets/Scripts/PlayerActionScript.cs
+++ Space Invader 146/Assets/Scripts/PlayerActionScript.cs
@@ -17,44 +17,36 @@ public class PlayerActionScript : MonoBehaviour {
 
 	void Update () {
 		if (!CounterScript.counter && !EnemyCounter.gameWin) {
-			if (Application.platform == RuntimePlatform.Android) {
-				transform.Translate (new Vector3 (Input.acceleration.x, 0f, 0f));
+			if (transform.position.x > -5.925f && Input.GetKey (KeyCode.A)) {
+				player.velocity = new Vector2 (-moveSpeed, 0);
+			} 
 
+			else if (transform.position.x < 5.925f && Input.GetKey (KeyCode.D)) {
+				player.velocity = new Vector2 (moveSpeed, 0);
+			}	
 
-				if (fireBullet > 0 && Input.touchCount > 0) {
-					fireBullet--;
-					float x = transform.position.x;
-					float y = transform.position.y + 0.35f;
-					laser = Instantiate (bullet, new Vector2 (x, y), Quaternion.identity);
-				}
+			else
+				player.velocity = Vector2.zero;
 
-				if (laser == null)
-					fireBullet = 1;
-			} else {
-				if (transform.position.x > -5.925f && Input.GetKey (KeyCode.A))
-					player.velocity = new Vector2 (-moveSpeed, 0);
-				else if (transform.position.x < 5.925f && Input.GetKey (KeyCode.D))
-					player.velocity = new Vector2 (moveSpeed, 0);
-				else
-					player.velocity = Vector2.zero;
-
-				if (fireBullet > 0 && Input.GetKeyDown (KeyCode.Space)) {
-					fireBullet--;
-					float x = transform.position.x;
-					float y = transform.position.y + 0.35f;
-					laser = Instantiate (bullet, new Vector2 (x, y), Quaternion.identity);
-				}
-
-				if (laser == null)
-					fireBullet = 1;
+			if (fireBullet > 0 && Input.GetKeyDown (KeyCode.Space)) {
+				fireBullet--;
+				float x = transform.position.x;
+				float y = transform.position.y + 0.35f;
+				laser = Instantiate (bullet, new Vector2 (x, y), Quaternion.identity);
 			}
-		} else {
+
+			if (laser == null){
+				fireBullet = 1;
+			}
+		} 
+
+		else {
 			player.velocity = Vector2.zero;
 		}
 	}
 
 	void OnTriggerEnter2D (Collider2D col) {
-		if (col.gameObject.tag == "EnemyBullet") {
+		if (col.gameObject.tag == "EnemyBullet"){
 			Destroy (col.gameObject);
 
 			LifeManager.lives--;
--- Space Invader 146/Assets/Scripts/UIScript.cs
+++ Space Invader 146/Assets/Scripts/UIScript.cs
@@ -9,21 +9,15 @@ public class UIScript : MonoBehaviour {
 	private Text startLabel;
 
 	void Start () {
-		startLabel = GetComponent<Text> ();
-
-		if (Application.platform == RuntimePlatform.Android)
-			startLabel.text = "Tap to Continue";
-		else
-			startLabel.text = "Press Start to Continue";
+		startLabel = GetComponent<Text>();
+		startLabel.text = "Press Start to Continue";
 	}
 
-	void Update () {
-		if (Application.platform == RuntimePlatform.Android) {
-			if (Input.touchCount > 0)
-				SceneManager.LoadScene ("main");
-		} else {
-			if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space))
-				SceneManager.LoadScene ("main");
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space))
+		{
+			SceneManager.LoadScene ("Main");
 		}
 	}
 }
